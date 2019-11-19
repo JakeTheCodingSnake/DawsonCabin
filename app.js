7b382fe0c01fc551b9ca22c3a86bf3d40d139e86
JakeTheCodingSnake <jake906@charter.net>
2019-11-18 19:51:02 -0600

    Removed lab13, finished home page, login page, and photos page

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
-$(".txtb input").on("focus", function() {
-  $(this).addClass("focus");
-});
-  
-$(".txtb input").on("blur", function() {
-  if($(this).val() == ""){
-    $(this).removeClass("focus");
-  }
-});
+//nav stuff
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+    //toggle nav
+    burger.addEventListener('click', ()=>{
+      nav.classList.toggle('nav-active');
+
+      //animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation){
+          link.style.animation = '';
+        }
+        else{
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+        }
+      });
+      //burger animation
+      burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+
+//SHOULD PROBABLY ADD A DIV TO THE LIGHTBOX WITH THE IMAGE INSIDE OF THAT
+//DIV, AND THEN WE CAN ADD THE DESCRIPTION TO THAT DIV AS WELL 
+var descriptions = [];
+descriptions[1] = "description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1";
+descriptions[2] = "description 2";
+descriptions[3] = "description 3";
+descriptions[4] = "description 4";
+descriptions[5] = "description 5";
+descriptions[6] = "description 6";
+descriptions[7] = "description 7";
+descriptions[8] = "description 8";
+descriptions[9] = "description 9";
+descriptions[10] = "description 10";
+descriptions[11] = "description 11";
+descriptions[12] = "description 12";
+descriptions[13] = "description 13";
+descriptions[14] = "description 14";
+
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        const description = document.createElement('span');
+        description.innerHTML = descriptions[image.id];
+
+        img.src = image.src;
+        while(lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
+        lightbox.appendChild(description);
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove('active');
+})
