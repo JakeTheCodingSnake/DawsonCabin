693e76002c9a01bdb87e6088f33e9f5e5e854529
jakedawson <dawsonjs7049@uwec.edu>
2019-11-21 07:00:06 -0600

    Have all pages done except for plan and cabsin

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 //nav stuff
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-    //toggle nav
-    burger.addEventListener('click', ()=>{
-      nav.classList.toggle('nav-active');
+  //toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-      //animate links
-      navLinks.forEach((link, index) => {
-        if(link.style.animation){
-          link.style.animation = '';
-        }
-        else{
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
-        }
-      });
-      //burger animation
-      burger.classList.toggle('toggle');
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.5}s`;
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
   });
-}
+};
 
 navSlide();
 
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-
-//SHOULD PROBABLY ADD A DIV TO THE LIGHTBOX WITH THE IMAGE INSIDE OF THAT
-//DIV, AND THEN WE CAN ADD THE DESCRIPTION TO THAT DIV AS WELL 
 var descriptions = [];
-descriptions[1] = "description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1";
+descriptions[1] =
+  "description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1";
 descriptions[2] = "description 2";
 descriptions[3] = "description 3";
 descriptions[4] = "description 4";
@@ -45,26 +41,32 @@ descriptions[12] = "description 12";
 descriptions[13] = "description 13";
 descriptions[14] = "description 14";
 
-document.body.appendChild(lightbox);
+var lightboxDiv = document.createElement("div");
+var lightbox = document.createElement("div");
+lightboxDiv.id = "lightboxDiv";
+lightbox.id = "lightbox";
 
-const images = document.querySelectorAll('img');
+lightboxDiv.appendChild(lightbox);
+document.body.appendChild(lightboxDiv);
+
+const images = document.querySelectorAll("img");
 images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img');
-        const description = document.createElement('span');
-        description.innerHTML = descriptions[image.id];
+  image.addEventListener("click", e => {
+    lightboxDiv.classList.add("active");
+    const img = document.createElement("img");
+    const description = document.createElement("div");
+    description.innerHTML = descriptions[image.id];
 
-        img.src = image.src;
-        while(lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild);
-        }
-        lightbox.appendChild(img);
-        lightbox.appendChild(description);
-    })
-})
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+    lightbox.appendChild(description);
+  });
+});
 
-lightbox.addEventListener('click', e => {
-    if(e.target !== e.currentTarget) return
-    lightbox.classList.remove('active');
-})
+lightboxDiv.addEventListener("click", e => {
+  if (e.target !== e.currentTarget) return;
+  lightboxDiv.classList.remove("active");
+});
