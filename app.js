eed5192870566f059c6ac000416e0a85632ba6b0
jakedawson <dawsonjs7049@uwec.edu>
2019-11-29 08:30:40 -0600

    add pictures + code updates

--- app.js
+++ app.js
@@ -26,20 +26,33 @@ navSlide();
 
 var descriptions = [];
 descriptions[1] =
-  "description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1description 1";
-descriptions[2] = "description 2";
-descriptions[3] = "description 3";
-descriptions[4] = "description 4";
-descriptions[5] = "description 5";
-descriptions[6] = "description 6";
-descriptions[7] = "description 7";
-descriptions[8] = "description 8";
-descriptions[9] = "description 9";
-descriptions[10] = "description 10";
-descriptions[11] = "description 11";
-descriptions[12] = "description 12";
-descriptions[13] = "description 13";
-descriptions[14] = "description 14";
+  "A view of our main lodge situated along the northwest corner of the lake.";
+descriptions[2] =
+  "The breathtaking Dinosaur Provincial Park in Alberta, known for its abundance of fossils!";
+descriptions[3] =
+  "A creek in Waterton Lakes National Park, Alberta. Known for its diverse wildlife.";
+descriptions[4] =
+  "A shallow creek part of Fish Creek National Park, one of the largest urban parks in North America!";
+descriptions[5] =
+  "Pigeon Coop cabin 4 in winter, situated along the northern coast of the lake.";
+descriptions[6] =
+  "Forillon National Park in Quebec, known for its unmatched vistas.";
+descriptions[7] =
+  "A neighboring cabin to our own Piegon Coob cabins, along the southern shore";
+descriptions[8] =
+  "Visitors to the Pacific Rim National Park in British Columbia";
+descriptions[9] =
+  "Another neighboring cabin situated along the eastern shore, picture taken in the spring.";
+descriptions[10] =
+  "A beach in Pukaskwa National Park, Ontario. This park has both camper-friendly shores and deep woods!";
+descriptions[11] =
+  "The unmatched view of mountains along the waterfront of Banff National Park";
+descriptions[12] =
+  "A bridge winding through the tall grasses of Point Pelee National Park, Ontario. Great for bird-watchers!";
+descriptions[13] =
+  "A bikepath along Stanley National Park, British Columbia. Abundant in views, trails, beaches and more!";
+descriptions[14] =
+  "Stunning view of a creek in Kootenay National Park, part of the Rocky Mountains.";
 
 var lightboxDiv = document.createElement("div");
 var lightbox = document.createElement("div");
